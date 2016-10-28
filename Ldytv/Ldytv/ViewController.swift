d10d740efa1752886e380c6b9b686b09cc99a3e2
honghui <honghui@honghuideiMac.local>
2016-10-28 16:43:36 +0800

    添加工程

---
+++ Ldytv/Ldytv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ldytv
+//
+//  Created by honghui on 16/10/28.
+//  Copyright © 2016年 honghui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
