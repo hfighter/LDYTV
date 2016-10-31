9951e88617d81f47eee6228ad7f683d2c9a77409
honghui <honghui@honghuideiMac.local>
2016-10-31 14:40:42 +0800

    搭建app框架

--- Ldytv/Ldytv/Classes/Main/MainTabBarController.swift
+++ Ldytv/Ldytv/Classes/Main/MainTabBarController.swift
@@ -1,25 +1,22 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  Ldytv
 //
-//  Created by honghui on 16/10/28.
+//  Created by honghui on 16/10/31.
 //  Copyright © 2016年 honghui. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
