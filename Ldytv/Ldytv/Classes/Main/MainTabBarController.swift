027dfe7e04a29d89f915064c734f370dd169369d
honghui <honghui@honghuideiMac.local>
2016-11-01 15:59:59 +0800

    拆分storyboard

--- Ldytv/Ldytv/Classes/Main/MainTabBarController.swift
+++ Ldytv/Ldytv/Classes/Main/MainTabBarController.swift
@@ -12,7 +12,7 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addControllers()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -22,6 +22,24 @@ class MainTabBarController: UITabBarController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+
+// MARK: - 添加tab子控制器
+extension MainTabBarController {
+    
+    fileprivate func addControllers() {
+        addControllerByStoryBoardName(storyboardName: "Home")
+        addControllerByStoryBoardName(storyboardName: "Live")
+        addControllerByStoryBoardName(storyboardName: "Follow")
+        addControllerByStoryBoardName(storyboardName: "Profile")
+    }
+    
+    private func addControllerByStoryBoardName(storyboardName: String) {
+        let instantiateViewControllerName: String = storyboardName + "NavViewController"
+        let viewController = UIStoryboard(name: storyboardName, bundle: nil).instantiateViewController(withIdentifier: instantiateViewControllerName)
+        addChildViewController(viewController)
     }
 }
 
